68e4b76c900af6dc555cff8e2133ef5a5da9ab87
kristoffer <180500@student.dtu.dk>
2022-06-28 16:06:09 +0200

    Initial Commit

---
+++ Shopping App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping App
+//
+//  Created by Kristoffer Baumgarten on 28/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
